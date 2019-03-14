1034491dcfa5825a5c75c0643b1daf4a2d7ad4bf
Sam-Ballantyne <25254611+Sam-Ballantyne@users.noreply.github.com>
2019-03-14 12:25:12 +0000

    Added form functionaility and basic validation

--- components/CreateTournamnetInput.js
+++ components/CreateTournamnetInput.js
@@ -1,34 +1,87 @@
 import React from 'react';
-import { Button, Text, View, Picker } from 'react-native';
+import { Button, Text, View, Picker, Alert } from 'react-native';
 import homeStyle from '../styles/homeStyle';
-import NumericInput from 'react-native-numeric-input';
 import { Input } from 'react-native-elements';
 import { withNavigation } from 'react-navigation';
 
 class CreateTournamentInput extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tournamentName: '',
+            size: '',
+            weight: '',
+            sex: 'male'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({ [evt.target.name]: evt.target.value });
+    }
+
+    handleSubmit(evt) {
+        console.log(this.state);
+        if (this.state.tournamentName === '' || this.state.size === '' || this.state.weight === '' || this.state.sex === '') {
+            Alert.alert(
+                'Please make sure you have entered all the details required'
+            )
+        } else if (this.state.size % 2 !== 0) {
+            Alert.alert(
+                'The tournament size must be a multiple of 2'
+            )
+        } else {
+            // Save the state as a new object and pass the ID of new object to page
+            this.props.navigation.navigate('Fighters');
+            title = "Create Tournament";
+        }
+    }
+
     render() {
         return (
             <View style={homeStyle.container}>
                 <View style={homeStyle.inputContainer}>
-                    <Input placeholder='Name' />
+                    <Input
+                        type='text'
+                        placeholder='Name'
+                        name='tournamentName'
+                        onChangeText={(val) => this.setState({ tournamentName: val })}
+                    />
                 </View>
                 <View style={homeStyle.inputContainer}>
-                    <Text style={homeStyle.infoText}>Size</Text>
-                    <NumericInput minValue={2} maxValue={64} onChange={value => console.log(value)} />
+                    <Input
+                        keyboardType='numeric'
+                        onChangeText={(val) => this.setState({ size: val })}
+                        maxLength={2}
+                        placeholder='Size'
+                        name='size'
+                    />
                 </View>
                 <View style={homeStyle.inputContainer}>
-                    <Text style={homeStyle.infoText}>Weight</Text>
-                    <NumericInput minValue={20} maxValue={200} onChange={value => console.log(value)} />
+                    <Input
+                        keyboardType='numeric'
+                        onChangeText={(val) => this.setState({ weight: val })}
+                        maxLength={6}
+                        placeholder='Weight(KG)'
+                        name='weight'
+                    />
                 </View>
-                <View style={homeStyle.inputContainer}>
-                    <Picker style={homeStyle.picker}>
-                        <Picker.Item label="Male" value="male" />
-                        <Picker.Item label="Female" value="female" />
+                <View style={homeStyle.inputContainer} >
+                    <Picker
+                        style={homeStyle.picker}
+                        name='sex'
+                        selectedValue={this.state.sex}
+                        onValueChange={(val, itemIndex) =>
+                            this.setState({ sex: val })
+                        }
+                    >
+                        <Picker.Item key="0" value="male" label="Male" />
+                        <Picker.Item key="1" value="female" label="Female" />
                     </Picker>
                 </View>
                 <View style={homeStyle.buttonContainer}>
                     <Button
-                        onPress={() => this.props.navigation.navigate('Fighters')}
+                        onPress={this.handleSubmit}
                         title="Create Tournament"
                     />
                 </View>
